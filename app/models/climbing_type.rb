6aa4ceaa1694423d58bc0a92c22b30c65b36cbca
dsauerbrun <dsauerbrun11@ucsbalum.com>
2015-03-29 22:56:21 -0700

    all s3 fixes

--- app/models/climbing_type.rb
+++ app/models/climbing_type.rb
@@ -1,5 +1,5 @@
 class ClimbingType < ActiveRecord::Base
-	has_attached_file :icon, :default_url => "/images/:style/missing.png"
+	has_attached_file :icon, :url => ':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
 	validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
 	has_and_belongs_to_many :locations
 end

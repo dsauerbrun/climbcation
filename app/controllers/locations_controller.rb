56af48550669fdf9c3c182b2621f8eba5ff8640c
dsauerbrun <dsauerbrun11@ucsbalum.com>
2015-05-13 14:36:40 -0700

    locations page api completed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,70 +1,13 @@
 class LocationsController < ApplicationController
 	def show
 		name_param= params[:slug]
+		return_map = {};
+		
 		@location = Location.where(slug: name_param).first
-		@close_locations = @location.get_locations_within_miles(200)
-		@map_locations = {}
-		@close_locations.each do |location|
-			@map_locations[location.id] = {}
-			@map_locations[location.id]['lat'] = location.latitude
-			@map_locations[location.id]['lng'] = location.longitude
-			@map_locations[location.id]['slug'] = location.slug
-			@map_locations[location.id]['name'] = location.name
-		end
-		@map_locations = @map_locations.to_json
-	end
-
-	def filter_locations_backup
-		#Location.joins(:seasons).where('seasons.id IN (?)',[1,3])	
-		#join filters = seasons,climbing_types
-		#non-join filters = continent, price range
-		#Location.where(continent: ['America', 'Europe'])
-		if(params[:filters].has_key? :continent)
-			continent_filter = params[:filters][:continent]
-		else	
-			continent_filter = Location.all.pluck(:continent).uniq 
-		end
-		if(params[:filters].has_key? :climbing_types)
-			climbing_filter = params[:filters][:climbing_types]
-		else	
-			climbing_filter = ClimbingType.all.pluck(:id) 
-		end
-		if(params[:filters][:price] == ["all"])
-			price_filter = 99999 
-		else
-			price_filter = params[:filters][:price].max
-		end
-		if(params.has_key? :sort)
-			if(params[:sort]=="price")
-				sort_filter = 'price_range_floor_cents ASC'
-			elsif(params[:sort]=="grade")
-				sort_filter = 'grade_id ASC'
-			else
-				sort_filter = 'name ASC'
-			end
-		end
-		location_list = {}
-		location_filter = Location.order(sort_filter).joins(:climbing_types).where('climbing_types.id IN (?)',climbing_filter).where(continent: continent_filter).where('price_range_floor_cents < ?',price_filter).includes(:grade,:seasons).uniq 
-		location_filter.each do |location|
-			puts location.climbing_types
-			location_list[location.name] = {}
-			location_list[location.name][:location] = location
-			location_list[location.name][:seasons] = {}
-			location_list[location.name][:home_thumb] = location.home_thumb.url
-			location.seasons.each do |season|
-				location_list[location.name][:seasons][season.id] = season.icon.url
-			end
-			location_list[location.name][:climbing_types] = {}
-			location.climbing_types.each do |climbing_type|
-				location_list[location.name][:climbing_types][climbing_type.id] = climbing_type.icon.url
-			end
-			location_list[location.name][:accommodations] = {}
-			location.accommodations.each do |accommodation|
-				location_list[location.name][:accommodations][accommodation.id] = accommodation.icon.url
-			end
-			location_list[location.name][:grade] = location.grade
-		end
-		render :json => location_list 
+		return_map['nearby'] = @location.get_nearby_locations_json
+		return_map['location'] = @location.get_location_json 
+		return_map['sections'] = @location.get_sections
+		render :json => return_map 
 	end
 
 	def filter_locations
@@ -98,41 +41,10 @@ class LocationsController < ApplicationController
 =end
 				sort_filter = 'name ASC'
 
-		puts continent_filter
-		puts climbing_filter
-		puts price_filter
 		location_filter = Location.order(sort_filter).joins(:climbing_types).where('climbing_types.name IN (?)',climbing_filter).where(continent: continent_filter).where('price_range_floor_cents < ?',price_filter).includes(:grade,:seasons).uniq 
 		#location_filter = Location.all.joins(:climbing_types).includes(:grade,:seasons).uniq 
 		location_filter.each do |location|
-			location_list[location.name] = {}
-			location_list[location.name][:location] = location
-			location_list[location.name][:slug] = location.slug
-			location_list[location.name][:name] = location.name
-			location_list[location.name][:country] = location.country
-			location_list[location.name][:price_range_floor_cents] = location.price_range_floor_cents
-			location_list[location.name][:price_range_ceiling_cents] = location.price_range_ceiling_cents
-
-			location_list[location.name][:home_thumb] = location.home_thumb.url
-			location_list[location.name][:seasons] = {}
-			location.seasons.each do |season|
-				location_list[location.name][:seasons][season.id] = {}
-				location_list[location.name][:seasons][season.id]['url'] = season.icon.url
-				location_list[location.name][:seasons][season.id]['name'] = season.name
-			end
-			location_list[location.name][:climbing_types] = {}
-			location.climbing_types.each do |climbing_type|
-				location_list[location.name][:climbing_types][climbing_type.id] = {}
-				location_list[location.name][:climbing_types][climbing_type.id]['url'] = climbing_type.icon.url
-				location_list[location.name][:climbing_types][climbing_type.id]['name'] = climbing_type.name
-
-			end
-			location_list[location.name][:accommodations] = {}
-			location.accommodations.each do |accommodation|
-				location_list[location.name][:accommodations][accommodation.id] = {}
-				location_list[location.name][:accommodations][accommodation.id]['url'] = accommodation.icon.url
-				location_list[location.name][:accommodations][accommodation.id]['name'] = accommodation.name
-			end
-			location_list[location.name][:grade] = location.grade.us
+			location_list[location.name] = location.get_location_json
 		end
 		render :json => location_list 
 	end
@@ -143,5 +55,6 @@ class LocationsController < ApplicationController
 		render :json => hello 
 
 	end
+
 end
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -33,4 +33,90 @@ class Location < ActiveRecord::Base
 		end
 	end
 
+	def get_accommodations
+		accommodations = {}
+		self.accommodations.each do |accommodation|
+			accommodations[accommodation.id] = {}
+			accommodations[accommodation.id]['url'] = accommodation.icon.url
+			accommodations[accommodation.id]['name'] = accommodation.name
+		end
+		return accommodations
+	end
+
+	def get_climbing_types
+		climbing_types = {}
+		self.climbing_types.each do |climbing_type|
+			climbing_types[climbing_type.id] = {}
+			climbing_types[climbing_type.id]['url'] = climbing_type.icon.url
+			climbing_types[climbing_type.id]['name'] = climbing_type.name
+		end
+		return climbing_types
+	end
+
+	def get_seasons
+		seasons = {}
+		self.seasons.each do |season|
+			seasons[season.id] = {}
+			seasons[season.id]['url'] = season.icon.url
+			seasons[season.id]['name'] = season.name
+		end
+		return seasons
+	end
+
+	def get_sections
+		sections = {}
+		self.info_sections.each do |section|
+			sections[section.id] = {}
+			sections[section.id][:title] = section.title
+			sections[section.id][:body] = section.body
+			sections[section.id][:data] = {} 
+			if section.metadata.present?
+				section.metadata.each do |key,metadata|
+					puts key
+					if(!sections[section.id][:data].has_key?(key))
+						sections[section.id][:data][key] = [] 
+					end
+					sections[section.id][:data][key] <<  metadata
+				end
+			end
+		end
+		puts sections
+		return sections
+
+	end
+
+	def get_location_json
+		json_return = {}
+		json_return[:location] = self
+		json_return[:latitude] = self.latitude
+		json_return[:longitude] = self.longitude
+		json_return[:slug] = self.slug
+		json_return[:name] = self.name
+		json_return[:country] = self.country
+		json_return[:price_range_floor_cents] = self.price_range_floor_cents
+		json_return[:price_range_ceiling_cents] = self.price_range_ceiling_cents
+		json_return[:home_thumb] = self.home_thumb.url
+		json_return[:seasons] = self.get_seasons
+		json_return[:climbing_types] = self.get_climbing_types
+		json_return[:accommodations] = self.get_accommodations 
+		json_return[:grade] = self.grade.us 
+		return json_return
+	end
+	
+	def get_nearby_locations_json
+		@close_locations = self.get_locations_within_miles(200)
+		@map_locations = {}
+		@close_locations.each do |location|
+			@map_locations[location.id] = {}
+			@map_locations[location.id]['lat'] = location.latitude
+			@map_locations[location.id]['lng'] = location.longitude
+			@map_locations[location.id]['slug'] = location.slug
+			@map_locations[location.id]['name'] = location.name
+			@map_locations[location.id]['country'] = location.country
+			@map_locations[location.id]['distance'] = self.distance_to(location).to_i
+
+		end
+		return @map_locations
+	end
+
 end

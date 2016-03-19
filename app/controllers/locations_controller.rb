a9f064c234ae18ccc2eb0e500ff5321a84d2c7f8
dsauerbrun <dsauerbrun11@ucsbalum.com>
2016-03-19 11:09:23 -0600

    expand text filter capabilities

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -87,7 +87,7 @@ class LocationsController < ApplicationController
 		location_filter = Location.where(active: true).order(sort_filter).in_bounds([@swBounds, @neBounds])
 			.joins(:seasons).where('seasons.numerical_value IN (?)', month_filter)
 			.joins(:climbing_types).where('climbing_types.name IN (?)',climbing_filter)
-			.joins('LEFT JOIN "info_sections" ON "info_sections"."location_id" = "locations"."id"').where('lower("info_sections"."body") LIKE lower(?) OR lower(array_dims(array["info_sections"."metadata"])) LIKE lower(?) OR lower("locations"."name") LIKE lower(?)',string_filter,string_filter,string_filter)
+			.joins('LEFT JOIN "info_sections" ON "info_sections"."location_id" = "locations"."id"').where('lower("info_sections"."body") LIKE lower(?) OR lower("locations"."name") LIKE lower(?) OR lower("locations"."getting_in_notes") LIKE lower(?) OR lower("locations"."accommodation_notes") LIKE lower(?) OR lower("locations"."common_expenses_notes") LIKE lower(?) OR lower("locations"."saving_money_tips") LIKE lower(?)',string_filter,string_filter,string_filter)
 			.where(continent: continent_filter)
 			.where('price_range_floor_cents < ?',price_filter)
 			.paginate(:page => page_num, :per_page => 100).uniq 

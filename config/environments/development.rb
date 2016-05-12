a23457ccfff5074dd5d9a3c818a8ddec837a2780
dsauerbrun <dsauerbrun11@ucsbalum.com>
2016-05-11 18:53:06 -0600

    dev settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+	config.active_record.logger = nil
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -34,4 +35,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+	config.paperclip_defaults = {
+	 :storage => :s3,
+		:url => ':s3_domain_url',
+		:path => '/:class/:attachment/:id_partition/:style/:filename',
+		:s3_credentials => {
+			:bucket => ENV['S3_BUCKET_NAME'],
+			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		}
+	}
+
 end

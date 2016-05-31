a11821e24f7f60492d0c967fad71d3e32470c68a
dsauerbrun <dsauerbrun11@ucsbalum.com>
2016-05-31 00:36:34 -0600

    pagination check

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -77,22 +77,7 @@ class LocationsController < ApplicationController
 		else
 			price_filter = 99999 
 		end
-
-
-=begin
-		unpaginated_filter = Location.select('locations.*, grades.order')
-			.where(active: true).in_bounds([@swBounds, @neBounds])
-			.joins(:grade)
-			.joins(:seasons).where('seasons.numerical_value IN (?)', month_filter)
-			.joins(:climbing_types).where('climbing_types.name IN (?)',climbing_filter)
-			.joins('LEFT JOIN "info_sections" ON "info_sections"."location_id" = "locations"."id"')
-			.where('lower("info_sections"."body") LIKE lower(?) OR lower("locations"."name") LIKE lower(?) OR lower("locations"."getting_in_notes") LIKE lower(?) OR lower("locations"."accommodation_notes") LIKE lower(?) OR lower("locations"."common_expenses_notes") LIKE lower(?) OR lower("locations"."saving_money_tips") LIKE lower(?)',string_filter,string_filter,string_filter,string_filter,string_filter,string_filter)
-			.where(continent: continent_filter)
-			.where('price_range_floor_cents < ?',price_filter)
-			.uniq
-=end
-
-		puts 'location filter here'
+		
 		location_filter = Location.select('locations.*, grades.order')
 			.where(active: true).in_bounds([@swBounds, @neBounds])
 			.joins(:grade)
@@ -141,9 +126,12 @@ class LocationsController < ApplicationController
 		locations_return[:paginated] = []
 		page_start = (page_num-1)*8
 		page_end = ((page_num-1)*8) + 7
-		location_filter[page_start..page_end].each do |location|
-			location_json = location.get_limited_location_json
-			locations_return[:paginated] << location_json
+		location_page = location_filter[page_start..page_end]
+		if !location_page.nil?
+			location_page.each do |location|
+				location_json = location.get_limited_location_json
+				locations_return[:paginated] << location_json
+			end
 		end
 		location_filter.each do |location|
 			location_json = location

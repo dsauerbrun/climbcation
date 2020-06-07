081d6402d8e0aae5f668c6039c50e4b82e2b3636
dsauerbrun <dsauerbrun@dsauerbruns-air.lan>
2020-06-07 10:04:18 -0600

    new front

---
+++ public/angularapp/precache-manifest.70abb899540662536d30b284d6f3c09b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4098185656d6768d0b5bb4bcd3cb6e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "815b9c12ce0f831b3fcb",
+    "url": "/static/css/main.7a722e4e.chunk.css"
+  },
+  {
+    "revision": "91fa067873e69cc5a8e6",
+    "url": "/static/js/2.2e58bdd9.chunk.js"
+  },
+  {
+    "revision": "13fd83076802c9154508a6ff63b511e8",
+    "url": "/static/js/2.2e58bdd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815b9c12ce0f831b3fcb",
+    "url": "/static/js/main.17028fe8.chunk.js"
+  },
+  {
+    "revision": "6d1999f551ff6a1e91a9",
+    "url": "/static/js/runtime-main.681b50f6.js"
+  },
+  {
+    "revision": "4b96bdbc50c2df3a16d599795ba88e9f",
+    "url": "/static/media/alpinerock.4b96bdbc.jpeg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5f1e7f9663e7501c056c31879f44225b",
+    "url": "/static/media/hero-image-left.5f1e7f96.png"
+  },
+  {
+    "revision": "0debb150d702aa037ff1b2e63f640a65",
+    "url": "/static/media/hero-image-right.0debb150.png"
+  },
+  {
+    "revision": "9ec541820eee12484b09ab17b4a2f677",
+    "url": "/static/media/rodellarpano.9ec54182.jpg"
+  },
+  {
+    "revision": "5d51711eb53589bf130631f2970f297f",
+    "url": "/static/media/summerNA.5d51711e.jpg"
+  }
+]);
